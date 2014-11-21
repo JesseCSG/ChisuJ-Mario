be19f9ff7f8bb625d0ef11d861ad057172b8d4f1
Jesse <jchisu31@gmail.com>
2014-11-21 09:19:58 -0800

    Mario-Update

--- public_html/js/screens/title.js
+++ public_html/js/screens/title.js
@@ -3,7 +3,9 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform on state change
 	 */
 	onResetEvent: function() {	
-		; // TODO
+		var titleImage = new me.Sprite(0, 0, me.loader.getImage("title-srceen"));
+                me.game.world.addChild(titleImage, 1);
+                me.input.bindKey(me.input.KEY.ENTER, "start");
 	},
 	
 	
